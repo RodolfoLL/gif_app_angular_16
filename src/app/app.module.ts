8243556fe997638e46059bfbcd0bc7f7b0f59f05
Rodolfo Lopez Lozano <201606416@gmail.com>
2024-06-23 23:26:29 -0400

    [UPDATE] validation and fetching api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GisfsModule } from './gisfs/gisfs.module';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     GisfsModule,
     SharedModule
   ],
--- src/app/services/gif.service.ts
+++ src/app/services/gif.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
@@ -5,7 +6,10 @@ import { Injectable } from '@angular/core';
 export class GifsService {
   //se pone en privado para que solo se pueda modificar desde esta clase
   private _tagHistory: string[] = [];
-  constructor() { }
+  private apiKey:string = 'HsWs96UjqER9mTpLHTuP14ApG6aMuTIj';
+  private serviceUrl:string = 'https://api.giphy.com/v1/gifs/search';
+
+  constructor(private http: HttpClient) { }
 
   get tagHistory(): string[] {
     //se manda la referencia del tagHistory
@@ -18,15 +22,24 @@ export class GifsService {
       return;
     }
     this.organaizedHistory(tag);
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag);
+    //TODO:FALTA COMPLETAR LA PETICION HTTP
+    this.http.get(`${this.serviceUrl}?api_key=${this.apiKey}&q=${tag}&limit=10`).subscribe((resp: any) => {
+      console.log(resp.data);
+    })
   }
 
   public organaizedHistory(tag: string): void {
     if (!this.tagHistory.includes(tag)) {
       this._tagHistory.unshift(tag);
     }else{
-      this._tagHistory = this.tagHistory.filter((value)=> value !== tag);
+      this._tagHistory = this.tagHistory.filter((oldTag)=> oldTag !== tag);
       this._tagHistory.unshift(tag);
     }
+    //la variable _tag_history solo admite 10 elementos
     if (this.tagHistory.length > 10) {
       this._tagHistory.pop();
     }
